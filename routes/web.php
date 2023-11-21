89c90f45866b07306de77ebd3923c79cc5ae3ca6
Gagan Deep Kashyap <cu.gagankashyap@gmail.com>
2023-11-21 09:54:03 +0530

    changes

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Vendor\Pdfcrowd\Pdfcrowd;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,4 +16,24 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/file', function () {
+    // Replace 'your-username' and 'your-api-key' with your Pdfcrowd credentials
+    $username = 'obligations';
+    $apiKey = '3258f988c032d72a74e8fb5feb83deb5';
+
+    $pdfcrowd = new Pdfcrowd($username, $apiKey);
+
+    $html = '<html><body><h1>Hello, Pdfcrowd!</h1></body></html>';
+
+    $options = [
+        'encoding' => 'utf-8',
+        'dpi' => 300,
+    ];
+
+    // Convert HTML to PDF
+    $pdf = $pdfcrowd->convertHtml($html, $options);
+
+    return response($pdf)->header('Content-Type', 'application/pdf');
 });
