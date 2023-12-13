1982a6a01d235866050378286981336de6c37937
gagankashyap <cu.gagankashyap@gmail.com>
2023-12-13 16:39:12 +0530

    dd

--- resources/views/calendar.blade.php
+++ resources/views/calendar.blade.php
@@ -2,7 +2,7 @@
 <html lang="en" >
 <head>
   <meta charset="UTF-8">
-  <title>CodePen - FullCalendar use to add event onclick with bootstrap 3 </title>
+  <title>Full Calendar By Gagan </title>
   <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/fullcalendar/2.1.1/fullcalendar.css'>
 <link rel='stylesheet' href='https://stackpath.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css'>
 
@@ -49,13 +49,12 @@
       </div>
       </form>
     </div>
-    <!-- /.modal-content -->
+
   </div>
-  <!-- /.modal-dialog -->
+
 </div>
-<!-- /.modal -->
-<!-- partial -->
-  <script src='https://code.jquery.com/jquery-3.3.1.min.js'></script>
+
+<script src='https://code.jquery.com/jquery-3.3.1.min.js'></script>
 <script src='https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.7.0/moment.min.js'></script>
 <script src='https://cdnjs.cloudflare.com/ajax/libs/fullcalendar/2.1.1/fullcalendar.min.js'></script>
 <script src='https://stackpath.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js'></script>
