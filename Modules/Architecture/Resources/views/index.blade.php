c9acaa111fe8d392ad4b5819767eda4d714bb560
Shiv <cu.gagankashyap@gmail.com>
2024-02-01 12:39:25 +0530

    all folder delted

--- Modules/Architecture/Resources/views/index.blade.php
+++
@@ -1,9 +0,0 @@
-@extends('architecture::layouts.master')
-
-@section('content')
-    <h1>Hello World</h1>
-
-    <p>
-        This view is loaded from module: {!! config('architecture.name') !!}
-    </p>
-@endsection
