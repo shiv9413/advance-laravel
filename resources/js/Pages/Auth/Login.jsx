5aa46ae7fd59314c005ceb584df39f08733e303e
Shiv <cu.gagankashyap@gmail.com>
2024-06-21 17:59:06 +0530

    login page added

--- resources/js/Pages/Auth/Login.jsx
+++ resources/js/Pages/Auth/Login.jsx
@@ -27,71 +27,57 @@ export default function Login({ status, canResetPassword }) {
     };
 
     return (
-        <GuestLayout>
-            <Head title="Log in" />
-
-            {status && <div className="mb-4 font-medium text-sm text-green-600">{status}</div>}
-
-            <form onSubmit={submit}>
-                <div>
-                    <InputLabel htmlFor="email" value="Email" />
-
-                    <TextInput
-                        id="email"
-                        type="email"
-                        name="email"
-                        value={data.email}
-                        className="mt-1 block w-full"
-                        autoComplete="username"
-                        isFocused={true}
-                        onChange={(e) => setData('email', e.target.value)}
-                    />
-
-                    <InputError message={errors.email} className="mt-2" />
+        // <GuestLayout>
+           <section className="ftco-section">
+                <div className="container">
+                    <div className="row justify-content-center">
+                        <div className="col-md-6 text-center mb-5">
+                            <h2 className="heading-section">Login</h2>
+                        </div>
+                    </div>
+                    <div className="row justify-content-center">
+                        <div className="col-md-6 col-lg-4">
+                            <div className="login-wrap p-0">
+                        <h3 className="mb-4 text-center">Have an account?</h3>
+                        <form action="#" className="signin-form">
+                            <div className="form-group">
+                                <input type="text" className="form-control" placeholder="Username" required />
+                            </div>
+                        <div className="form-group">
+                        <input id="password-field" type="password" className="form-control" placeholder="Password" required />
+                        <span toggle="#password-field" className="fa fa-fw fa-eye field-icon toggle-password"></span>
+                        </div>
+                        <div className="form-group">
+                            <button type="submit" className="form-control btn btn-primary submit px-3">Sign In</button>
+                        </div>
+                        <div className="form-group d-md-flex">
+                            <div className="w-50">
+                                <label className="checkbox-wrap checkbox-primary">Remember Me
+                                            <input type="checkbox" checked />
+                                            <span className="checkmark"></span>
+                                            </label>
+                                        </div>
+                                        <div className="w-50 text-md-right">
+                                        <Link
+                                            href="#"
+                                            className="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                                        >
+                                            Forgot your password?
+                                        </Link>
+                                            {/* <a href="#" style="color: #fff">Forgot Password</a> */}
+                                        </div>
+                        </div>
+                    </form>
+                    <p className="w-100 text-center">&mdash; Or Sign In With &mdash;</p>
+                    <div className="social d-flex text-center">
+                        <Link href="" className="px-2 py-2 mr-md-1 rounded"><span className="ion-logo-facebook mr-2"></span>Facebook</Link>
+                        <Link href="" className="px-2 py-2 mr-md-1 rounded"><span className="ion-logo-twitter mr-2"></span>Twitter</Link>
+                    </div>
+                    </div>
+                        </div>
+                    </div>
                 </div>
-
-                <div className="mt-4">
-                    <InputLabel htmlFor="password" value="Password" />
-
-                    <TextInput
-                        id="password"
-                        type="password"
-                        name="password"
-                        value={data.password}
-                        className="mt-1 block w-full"
-                        autoComplete="current-password"
-                        onChange={(e) => setData('password', e.target.value)}
-                    />
-
-                    <InputError message={errors.password} className="mt-2" />
-                </div>
-
-                <div className="block mt-4">
-                    <label className="flex items-center">
-                        <Checkbox
-                            name="remember"
-                            checked={data.remember}
-                            onChange={(e) => setData('remember', e.target.checked)}
-                        />
-                        <span className="ms-2 text-sm text-gray-600">Remember me</span>
-                    </label>
-                </div>
-
-                <div className="flex items-center justify-end mt-4">
-                    {canResetPassword && (
-                        <Link
-                            href={route('password.request')}
-                            className="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-                        >
-                            Forgot your password?
-                        </Link>
-                    )}
-
-                    <PrimaryButton className="ms-4" disabled={processing}>
-                        Log in
-                    </PrimaryButton>
-                </div>
-            </form>
-        </GuestLayout>
+            </section>
+        //</GuestLayout>
     );
 }
--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -9,6 +9,13 @@
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+        
+        <!-- Extra -->
+        <link href="https://fonts.googleapis.com/css?family=Lato:300,400,700&display=swap" rel="stylesheet">
+
+	    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+
+	    <link rel="stylesheet" href="{{ asset('custom/style.css')}}">
 
         <!-- Scripts -->
         @routes
@@ -16,7 +23,11 @@
         @vite(['resources/js/app.jsx', "resources/js/Pages/{$page['component']}.jsx"])
         @inertiaHead
     </head>
-    <body class="font-sans antialiased">
+    <body class="img js-fullheight font-sans antialiased" style="background-image: url({{asset('custom/images/bg.jpg')}});">
         @inertia
     </body>
+    <script src="{{ asset('custom/jquery.min.js') }}"></script>
+    <script src="{{ asset('custom/popper.js') }}"></script>
+    <script src="{{ asset('custom/bootstrap.min.js') }}"></script>
+    <script src="{{ asset('custom/main.js') }}"></script>
 </html>
